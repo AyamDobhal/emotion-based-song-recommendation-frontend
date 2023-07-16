17c9d6b8dcb709976ebf397fe6b5ccc9f68ba459
Neeraj <neerajgodiyal250@gmail.com>
2023-07-16 08:36:08 +0530

    input history svg

--- src/styles/components/Header.js
+++ src/styles/components/Header.js
@@ -9,15 +9,16 @@ export const MainHeader = styled.div`
   background-color: #4169e1;
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
-  gap: 1rem;
+  gap: 40rem;
   z-index: 2;
 
   nav {
     display: flex;
     flex-direction: row;
     gap: 1rem;
+    
   }
 
   nav > a {
@@ -27,16 +28,47 @@ export const MainHeader = styled.div`
   }
 
   nav > a:hover {
-    text-decoration:underline;
+    text-decoration: underline;
     text-decoration-color: #ddd;
   }
 
+  .song {
+    font-weight: bolder;
+    color: #f2f2f2;
+    text-decoration: none;
+  }
+
+  .digital_india {
+    width: 80px;
+    height: 80px;
+    justify-cotent: center;
+    align-items: center;
+    margin-right: 60px;
+  }
+
+  .flag-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+  }
+
+  .ashok_chakra {
+    width: 30px;
+    height: 30px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    margin-right: 30px;
+    
+  }
 
   @media (max-width: 400px) {
     height: 8%;
 
-    nav, h3 {
+    nav,
+    h3 {
       padding: 1rem;
     }
   }
-`
+`;
